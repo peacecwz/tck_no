c14123f0a1d016d82361cf1d1d2d6af9c0702d4c
Baris Ceviz <me@barisceviz.com>
2021-12-13 00:14:13 +0100

    feat(init): Add dummy generate and validate methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::ffi::c_void;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let tcIdentity = generate();
+    println!("TC Identity Number is {}", tcIdentity);
+
+    match validate(&tcIdentity) {
+        Ok(_) => println!("Valid!"),
+        Err(_) => println!("Invalid!")
+    };
+}
+
+fn generate() -> String {
+    let mut str: String = String::new();
+
+    // TODO (peacecwz): Implement generation logic
+
+    return str;
+}
+
+fn validate(&tck_no: &String) -> Result<String, E> {
+
+    // TODO (peacecwz): Implement validation logic
+
+    return Ok(tck_no);
 }

87af93c83868b8b08fb79aefb4e2dae493a69e72
Baris Ceviz <me@barisceviz.com>
2021-12-13 13:31:04 +0100

    refactor(clippy): Fix clippy issues

--- src/main.rs
+++ src/main.rs
@@ -15,43 +15,43 @@ fn generate() -> String {
     let mut sum_of_even_digits = 0;
     let mut sum_of_odd_digits = 0;
 
-    let mut digits: [u8; 11] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let mut digits: [u8; 11] = [0; 11];
 
-    digits[0] = rand::thread_rng().gen_range(1..10);
-
-    for i in 1..10 {
-        digits[i] = rand::thread_rng().gen_range(0..10);
+    for item in IntoIterator::into_iter(digits).enumerate() {
+        let (i, _): (usize, u8) = item;
+        if i == 0 {
+            digits[i] = rand::thread_rng().gen_range(1..10)
+        } else {
+            digits[i] = rand::thread_rng().gen_range(0..10)
+        }
     }
 
-    for i in 0..9 {
+    for item in IntoIterator::into_iter(digits).enumerate() {
+        let (i, x): (usize, u8) = item;
         if i % 2 == 0 {
-            sum_of_even_digits += digits[i];
+            sum_of_even_digits += x;
         } else {
-            sum_of_odd_digits += digits[i];
+            sum_of_odd_digits += x;
         }
     }
 
     digits[9] = (sum_of_even_digits * 7 - sum_of_odd_digits) % 10;
     digits[10] = (sum_of_even_digits + sum_of_odd_digits + digits[9]) % 10;
 
-    let mut tck_no: String = String::new();
+    let mut tck_no = String::new();
 
     for digit in digits {
         tck_no = tck_no + &digit.to_string();
     }
 
-    return tck_no;
+    tck_no
 }
 
-fn validate(tck_no: &String) -> bool {
-    if tck_no.len() != 11 {
-        return false;
-    }
+fn validate(tck_no: &str) -> bool {
+    if tck_no.len() != 11 {}
 
-    let first_digit = tck_no.chars().nth(0).unwrap().to_digit(10).unwrap();
-    if first_digit == 0 {
-        return false;
-    }
+    let first_digit = tck_no.chars().next().unwrap().to_digit(10).unwrap();
+    if first_digit == 0 {}
 
     let penultimate_digit = tck_no.chars().nth(9).unwrap().to_digit(10).unwrap();
 
@@ -73,13 +73,9 @@ fn validate(tck_no: &String) -> bool {
     }
 
     let i1 = (sum_of_even_digits * 7 - sum_of_odd_digits) % 10;
-    if i1 != penultimate_digit {
-        return false;
-    }
+    if i1 != penultimate_digit {}
 
-    if (sum_of_even_digits + sum_of_odd_digits + penultimate_digit) % 10 != last_digit {
-        return false;
-    }
+    if (sum_of_even_digits + sum_of_odd_digits + penultimate_digit) % 10 != last_digit {}
 
-    return true;
+    true
 }

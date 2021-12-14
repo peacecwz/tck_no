41fb584bea1806066bdbc0da934e57f6dafb1aa9
Baris Ceviz <me@barisceviz.com>
2021-12-14 23:43:07 +0100

    refactor(performance): Refactor loops
    Thanks to @bthaydin for suggestions and refactors

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,15 @@ pub mod tckn {
         let mut sum_of_odd_digits = 0;
 
         let mut digits: [u8; 11] = [0; 11];
+        digits[0] = rand::thread_rng().gen_range(1..10);
 
-        for item in IntoIterator::into_iter(digits).enumerate() {
-            let (i, _): (usize, u8) = item;
-            if i == 0 {
-                digits[i] = rand::thread_rng().gen_range(1..10)
-            } else {
-                digits[i] = rand::thread_rng().gen_range(0..10)
-            }
+        for digit in digits.iter_mut().skip(1) {
+            *digit = rand::thread_rng().gen_range(0..10);
         }
 
-        for item in IntoIterator::into_iter(digits).enumerate() {
-            let (i, x): (usize, u8) = item;
+        for item in digits.iter().take(9).enumerate() {
+            let (i, x) = item;
+
             if i % 2 == 0 {
                 sum_of_even_digits += x;
             } else {
@@ -28,20 +25,24 @@ pub mod tckn {
         digits[9] = (sum_of_even_digits * 7 - sum_of_odd_digits) % 10;
         digits[10] = (sum_of_even_digits + sum_of_odd_digits + digits[9]) % 10;
 
-        let mut tck_no = String::new();
+        let mut tck_no = String::with_capacity(11);
 
         for digit in digits {
-            tck_no = tck_no + &digit.to_string();
+            tck_no.push_str(&digit.to_string());
         }
 
         tck_no
     }
 
     pub fn validate(tck_no: &str) -> bool {
-        if tck_no.len() != 11 {}
+        if tck_no.len() != 11 {
+            return false;
+        }
 
         let first_digit = tck_no.chars().next().unwrap().to_digit(10).unwrap();
-        if first_digit == 0 {}
+        if first_digit == 0 {
+            return false;
+        }
 
         let penultimate_digit = tck_no.chars().nth(9).unwrap().to_digit(10).unwrap();
 
@@ -51,21 +52,25 @@ pub mod tckn {
         let mut sum_of_odd_digits = 0;
 
         for i in 0..10 {
-            let d = tck_no.chars().nth(i).unwrap().to_digit(10).unwrap();
+            let digit = tck_no.chars().nth(i).unwrap().to_digit(10).unwrap();
 
             if i % 2 == 0 && i <= 8 {
-                sum_of_even_digits += d;
+                sum_of_even_digits += digit;
             }
 
             if i % 2 != 0 && i < 8 {
-                sum_of_odd_digits += d
+                sum_of_odd_digits += digit;
             }
         }
 
         let i1 = (sum_of_even_digits * 7 - sum_of_odd_digits) % 10;
-        if i1 != penultimate_digit {}
+        if i1 != penultimate_digit {
+            return false;
+        }
 
-        if (sum_of_even_digits + sum_of_odd_digits + penultimate_digit) % 10 != last_digit {}
+        if (sum_of_even_digits + sum_of_odd_digits + penultimate_digit) % 10 != last_digit {
+            return false;
+        }
 
         true
     }
--- tests/lib.rs
+++ tests/lib.rs
@@ -3,8 +3,12 @@ mod tests {
     use tck_no::tckn::{generate, validate};
 
     #[test]
-    fn it_should_generate_tckn() {
-        let result = generate();
-        assert!(validate(&result));
+    fn it_should_generate_tck_no() {
+        let tck_no = generate();
+
+        println!("TC Identity Number: {}", tck_no);
+
+        let result = validate(&tck_no);
+        assert!(result);
     }
 }
